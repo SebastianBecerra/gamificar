9876ffaec6eb528150a5a6f3a3356f23092c740d
Sebastian Becerra <SebastianBecerra@users.noreply.github.com>
2018-03-22 15:43:56 -0300

    Prueba ej5
    funcinalidad ej5

---
+++ matematica_primer_grado/Assets/Scenes/ejercicio5ParaElClaudio/script/mat1ej5clickManager.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mat1ej5clickManager : MonoBehaviour
+{
+
+    mat1ej5controller clicker;
+    popUpScript popUP;
+
+    // Use this for initialization
+    void Start()
+    {
+        clicker = FindObjectOfType<mat1ej5controller>();
+        popUP = FindObjectOfType<popUpScript>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnMouseDown()
+    {
+        if (clicker.estado < 3)
+        {
+            if (gameObject.transform.tag == "Player"&&gameObject.GetComponent<Carta>())
+            {
+                popUP.cartelAcierto();
+                clicker.estado++;
+                gameObject.GetComponent<Carta>().enabled = false;
+                gameObject.GetComponent<InteractuableScale>().enabled = false;
+                if (clicker.estado == 3)
+                {
+                    Invoke("cambiarBandera", 0.5f);
+                }
+            }
+
+            else
+            {
+                popUP.Mal();
+            }
+        }
+        if (clicker.banderaUltimoPar)
+        {
+            if (gameObject.transform.tag == "Player")
+            {
+                gameObject.GetComponent<Carta>().enabled = false;
+                gameObject.GetComponent<InteractuableScale>().enabled = false;
+                popUP.Bien();
+            }
+            else
+            {
+                popUP.Mal();
+            }
+        }
+    }
+
+    void cambiarBandera()
+    {
+        clicker.banderaUltimoPar = true;
+    }
+}
+
---
+++ matematica_primer_grado/Assets/Scenes/ejercicio5ParaElClaudio/script/mat1ej5controller.cs
@@ -0,0 +1,191 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class mat1ej5controller : MonoBehaviour {
+
+    public Carta[] cartas;
+    private bool igualPar1, igualPar2, igualPar3, igualPar4;
+    popUpScript popUp;
+
+    public int estado;
+    public bool banderaUltimoPar = false;
+
+    // Use this for initialization
+    void Start() {
+        cartas = FindObjectsOfType<Carta>().OrderBy(go => go.name).ToArray();
+        popUp= FindObjectOfType<popUpScript>();
+        darValores();
+        comparacion();
+        estado = 0;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void darValores()
+    {
+        foreach (Carta a in cartas)
+        {
+            a.valor = Random.Range(1, 9);
+        }
+    }
+
+    void comparacion()
+    {
+        //par1
+        if (cartas[0].valor != cartas[1].valor)
+        {
+            if (cartas[0].valor > cartas[1].valor)
+            {
+                cartas[0].transform.tag = "Player";
+            }
+            else
+            {
+                cartas[1].transform.tag = "Player";
+            }
+        }
+        else
+        {
+            igualPar1 = true;
+        }
+        //par2
+        if (cartas[2].valor != cartas[3].valor)
+        {
+            if (cartas[2].valor > cartas[3].valor)
+            {
+                cartas[2].transform.tag = "Player";
+            }
+            else
+            {
+                cartas[3].transform.tag = "Player";
+            }
+        }
+        else
+        {
+            igualPar2 = true;
+        }
+        //par3
+        if (cartas[4].valor != cartas[5].valor)
+        {
+            if (cartas[4].valor > cartas[5].valor)
+            {
+                cartas[4].transform.tag = "Player";
+            }
+            else
+            {
+                cartas[5].transform.tag = "Player";
+            }
+        }
+        else
+        {
+            igualPar3 = true;
+        }
+        //par4
+        if (cartas[6].valor != cartas[7].valor)
+        {
+            if (cartas[6].valor > cartas[7].valor)
+            {
+                cartas[6].transform.tag = "Player";
+            }
+            else
+            {
+                cartas[7].transform.tag = "Player";
+            }
+        }
+        else
+        {
+            igualPar4 = true;
+        }
+    }
+
+    public void btnIgualPar1()
+    {
+        if (igualPar1)
+        {
+            popUp.cartelAcierto();
+            estado++;
+            cartas[0].GetComponent<Carta>().enabled = false;
+            cartas[1].GetComponent<Carta>().enabled = false;
+            cartas[0].GetComponent<InteractuableScale>().enabled = false;
+            cartas[1].GetComponent<InteractuableScale>().enabled = false;
+            if (estado == 4)
+            {
+                popUp.Bien();
+            }
+        }
+        else
+        {
+            popUp.Mal();
+        }
+    }
+
+    public void btnIgualPar2()
+    {
+        if (igualPar2)
+        {
+            popUp.cartelAcierto();
+            estado++;
+            cartas[2].GetComponent<Carta>().enabled = false;
+            cartas[3].GetComponent<Carta>().enabled = false;
+            cartas[2].GetComponent<InteractuableScale>().enabled = false;
+            cartas[3].GetComponent<InteractuableScale>().enabled = false;
+            if (estado == 4)
+            {
+                popUp.Bien();
+            }
+        }
+        else
+        {
+            popUp.Mal();
+        }
+    }
+
+    public void btnIgualPar3()
+    {
+        if (igualPar3)
+        {
+            popUp.cartelAcierto();
+            estado++;
+            cartas[4].GetComponent<Carta>().enabled = false;
+            cartas[5].GetComponent<Carta>().enabled = false;
+            cartas[4].GetComponent<InteractuableScale>().enabled = false;
+            cartas[5].GetComponent<InteractuableScale>().enabled = false;
+            if (estado == 4)
+            {
+                popUp.Bien();
+            }
+        }
+        else
+        {
+            popUp.Mal();
+        }
+    }
+
+    public void btnIgualPar4()
+    {
+        if (igualPar4)
+        {
+            if (estado == 4)
+            {
+                popUp.Bien();
+            }
+            else
+            {
+                popUp.cartelAcierto();
+                estado++;
+                cartas[6].GetComponent<Carta>().enabled = false;
+                cartas[7].GetComponent<Carta>().enabled = false;
+                cartas[6].GetComponent<InteractuableScale>().enabled = false;
+                cartas[7].GetComponent<InteractuableScale>().enabled = false;
+            }
+        }
+        else
+        {
+            popUp.Mal();
+        }
+    }
+}
