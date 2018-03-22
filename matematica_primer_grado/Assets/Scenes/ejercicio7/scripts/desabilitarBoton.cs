9322fbdebc437b8a45820b6feaa00ca03b983540
Sebastian Becerra <SebastianBecerra@users.noreply.github.com>
2018-03-22 11:55:52 -0300

    Ejercicio 7
    Terminada escena10, faltantes de gradualidad

---
+++ matematica_primer_grado/Assets/Scenes/ejercicio7/scripts/desabilitarBoton.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class desabilitarBoton : MonoBehaviour
+{
+    //script con la funcionalidad de los botones de reproducir sonido y identificar si se encuentra o no en el carton
+
+    private Button btn;//referencia al boton reproducir sonido donde se encuentra este script
+    private GameObject btn_noEsta,btn2; //referencia al boton que indica que el sonido no esta
+    public AudioClip sonidoNoEsta; //referencai al sonido que no se encuentra en la escena
+    public AudioClip sonidoSiguiente; //sonido que sigue si se acierta
+    private AudioClip sonidoAux; //variable que compara el sonido con el que no se encuentra
+    private AudioSource sonido; //referencia al audiosource del boton de reproducir sonido
+    popUpScript popUp; //referencia a los popups
+    // Use this for initialization
+    void Start()
+    {
+        //asignacion de las variables
+        btn = gameObject.GetComponent<Button>();
+        btn2 = GameObject.FindGameObjectWithTag("Player");
+        btn_noEsta = GameObject.FindGameObjectWithTag("Cuadrado");
+        sonido = GameObject.FindGameObjectWithTag("Player").GetComponent<AudioSource>();
+        popUp = FindObjectOfType<popUpScript>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    //metodo del boton reproducir sonido 
+    public void unabled()
+    {
+        AudioSource audio = GetComponent<AudioSource>();//reproduce el primer audio seteado
+        audio.Play();
+        sonidoAux = btn.GetComponent<AudioSource>().clip; //lo guarda para comparacion
+        btn.interactable = false; //se hace no interactuable hasta que acierte si se encuentra ese numero o no
+        btn_noEsta.GetComponent<desabilitarBoton>().sonidoAux = sonidoAux; //le asigna el sonido auxiliar al otro boton para comparar
+    }
+
+    //metodo para el boton que identifica si el numero reproducido no se encuentra en el carton
+    public void noEsta()
+    {
+        if (btn_noEsta.GetComponent<desabilitarBoton>().sonidoAux == sonidoNoEsta)//si la comparacion es verdadera significa que
+        {                                                                         //el numero reproducido es el que no se encuentra
+            popUp.cartelAcierto(); //activa el cartel de acierto
+            sonido.clip = sonidoSiguiente; //asigna el sonido siguiente
+            //btn.interactable = true;
+            btn2.GetComponent<Button>().interactable = true; //el boton vuelve a ser interactuable
+        }
+        else
+        {
+            popUp.Mal();//si falla la comparacion, aparece el cartel de intentar denuevo
+        }
+    }
+}
---
+++ matematica_primer_grado/Assets/Scenes/ejercicio7/scripts/numero2.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class numero2 : MonoBehaviour
+{
+
+    public GameObject btn;
+    public AudioSource sonido;
+    public AudioClip sonidoSiguiente;
+    popUpScript popUp;
+    changeSprite spr;
+    public int estado;
+    private int i = 0;
+    // Use this for initialization
+    void Start()
+    {
+        estado = 0;
+        btn = GameObject.FindGameObjectWithTag("Player");
+        sonido = GameObject.FindGameObjectWithTag("Player").GetComponent<AudioSource>();
+        spr = gameObject.GetComponent<changeSprite>();
+        popUp = FindObjectOfType<popUpScript>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnMouseDown()
+    {
+        if (btn.GetComponent<Button>().interactable == true)
+        {
+            popUp.Mal();
+        }
+        else
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            // Casts the ray and get the first game object hit
+            RaycastHit hit;
+            Physics.Raycast(ray, out hit);
+            if (hit.transform.name == "numero2" && sonido.clip.name == "voz-tres")
+            {
+                Debug.Log("numero 2 bien");
+                popUp.cartelAcierto();
+                sonido.clip = sonidoSiguiente;
+                spr.enabled = false;
+
+            }
+            else
+            {
+                Debug.Log("PERDIO JUEGO");
+                popUp.Mal();
+            }
+            btn.GetComponent<Button>().interactable = true;
+        }
+    }
+}
