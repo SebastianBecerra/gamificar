f3fdb560ec12fb8f2950cbee2ef936d186cd0fe5
Sebastian Becerra <SebastianBecerra@users.noreply.github.com>
2018-03-25 16:45:20 -0300

    Edit ej7
    Añadido comentqaqrios a script m1ej7numHandler

--- matematica_primer_grado/Assets/Scenes/ejercicio7/scripts/m1ej7numHandler.cs
+++ matematica_primer_grado/Assets/Scenes/ejercicio7/scripts/m1ej7numHandler.cs
@@ -5,15 +5,16 @@ using UnityEngine.UI;
 
 public class m1ej7numHandler : MonoBehaviour {
 
-    public GameObject btn;
-    public AudioSource sonido;
-    public int cantNumeros;
-    popUpScript popUp;
-    public AudioClip sonidoSiguiente;
-    changeSprite spr;
+    public GameObject btn; //referencia al btn de reproducir sondo
+    public AudioSource sonido; //referencia al audio
+    public int cantNumeros; //cantidad de numeros que tiene la plantilla
+    popUpScript popUp;//referencia a los popups
+    public AudioClip sonidoSiguiente; //siguiente sonido despues de acertar
+    changeSprite spr;//referencia para cambiar sprites on click
 
     // Use this for initialization
     void Start () {
+        //asignacion de variables
         btn = GameObject.FindGameObjectWithTag("Player");
         sonido = GameObject.FindGameObjectWithTag("Player").GetComponent<AudioSource>();
         spr = gameObject.GetComponent<changeSprite>();
@@ -26,7 +27,7 @@ public class m1ej7numHandler : MonoBehaviour {
 	}
     private void OnMouseDown()
     {
-        if (gameObject.transform.name == "noClick")
+        if (gameObject.transform.name == "noClick")//si el nombre de el objeto que clickeo es noClick no sucede nada
         {
             return;
         }
@@ -36,35 +37,36 @@ public class m1ej7numHandler : MonoBehaviour {
         }
         else
         {
-            if (cantNumeros == btn.GetComponent<desabilitarBoton>().aux-1)
+            if (cantNumeros == btn.GetComponent<desabilitarBoton>().aux-1)//si es el ultimo numero a acertar
             {
-                if (gameObject.transform.name == sonido.clip.name)
+                if (gameObject.transform.name == sonido.clip.name)//si el nombre del sonido y del objeto concuerdan
                 {
-                    popUp.Bien();
+                    popUp.Bien();//gano el juego
                     gameObject.transform.name = "noClick";
                 }
-                else
+                else//sino sale el cartel de intentar de nuevo
                 {
                     popUp.Mal();
                     gameObject.transform.name = "noClick";
                 }
             }
-            else
+            else//sino es el ultimo numero a acertar
             {
-                if (gameObject.transform.name == sonido.clip.name)
+                if (gameObject.transform.name == sonido.clip.name)//si el nombre del sonido y del objeto concuedan
                 {
-                    popUp.cartelAcierto();
-                    sonido.clip = sonidoSiguiente;
-                    spr.enabled = false;
-                    gameObject.transform.name = "noClick";
+                    popUp.cartelAcierto();//activo el cartel de acierto
+                    sonido.clip = sonidoSiguiente;//asigno el siguiente sonido a acertar
+                    spr.enabled = false;//desabilito la funcionalidad de cambiar sprite on click
+                    gameObject.transform.name = "noClick";//cambio el nombre para que no suceda nada si lo clickeo nuevamente
                 }
-                else
+                else//sino
                 {
-                    popUp.Mal();
+                    popUp.Mal();//sale el cartel de intentar de nuevo
                     gameObject.transform.name = "noClick";
                 }
             }
-            btn.GetComponent<Button>().interactable = true;
+            btn.GetComponent<Button>().interactable = true;//el boton de reproducir sonido vuelve a ser interactuable para
+                                                           //obtener el siguiente sonido a acertar
         }
     }
 }
