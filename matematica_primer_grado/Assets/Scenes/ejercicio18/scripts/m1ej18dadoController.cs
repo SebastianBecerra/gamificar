b33ece222563b35946115fc8d5127743cfba900a
Sebastian Becerra <SebastianBecerra@users.noreply.github.com>
2018-03-27 12:22:24 -0300

    ejercicio 18
    ejercicio tabla con dos dados - incompleto -

---
+++ matematica_primer_grado/Assets/Scenes/ejercicio18/scripts/m1ej18dadoController.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
+
+public class m1ej18dadoController : MonoBehaviour {
+
+    public GameObject[] textosDado1;
+    public GameObject[] textosDado2;
+    public GameObject[] textos;
+    public Button[] botones;
+    private int randomInt;
+    private int i, j;
+
+	// Use this for initialization
+	void Start () {
+        textos = GameObject.FindGameObjectsWithTag("input1").OrderBy(go => go.name).ToArray();
+        textosDado1 = GameObject.FindGameObjectsWithTag("textoDado1").OrderBy(go => go.name).ToArray();
+        textosDado2 = GameObject.FindGameObjectsWithTag("textoDado2").OrderBy(go => go.name).ToArray();
+        i = 0;
+        j = 0;
+        darValores();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void dado1()
+    {
+        randomInt = Random.Range(1, 6);
+        textosDado1[i].GetComponent<Text>().text = randomInt.ToString();
+        i++;
+        if (i == 4)
+        {
+            botones[0].interactable = false;
+        }
+    }
+
+    public void dado2()
+    {
+        randomInt = Random.Range(1, 6);
+        textosDado2[j].GetComponent<Text>().text = randomInt.ToString();
+        j++;
+        if (j == 4)
+        {
+            botones[1].interactable = false;
+        }
+    }
+
+    void darValores()
+    {
+        foreach(GameObject a in textos)
+        {
+            a.GetComponent<Text>().text = Random.Range(1, 33).ToString();
+        }
+    }
+
+}
---
+++ matematica_primer_grado/Assets/Scenes/ejercicio18/scripts/m1ej18resultController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
+
+public class m1ej18resultController : MonoBehaviour {
+
+    public GameObject[] results;
+    m1ej18dadoController num;
+
+	// Use this for initialization
+	void Start () {
+        results = GameObject.FindGameObjectsWithTag("input2").OrderBy(go => go.name).ToArray();
+        num = FindObjectOfType<m1ej18dadoController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void controlarResultados()
+    {
+
+    }
+}
