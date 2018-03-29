57a41c63298fbe297cb5963b54cee33b5f795e02
ClaudioOlivares <claudio_05_028@hotmail.com>
2018-03-29 17:34:18 -0300

    Ejercicio 15
    Se agregaron 4 scenes y 2 scripts

---
+++ matematica_primer_grado/Assets/Scripts/checkewinejercicio15.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class checkewinejercicio15 : MonoBehaviour {
+	public scriptejercicio15 [] array;
+	private scriptejercicio15 aux;
+	public popUpScript popup;
+	private bool win;
+	public int clickeados;
+	public int correctos;
+	public int opciones;
+	// Use this for initialization
+	void Start () {
+		array = FindObjectsOfType<scriptejercicio15> ();
+		popup = FindObjectOfType<popUpScript> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		win = checkeo ();
+		if (opciones == correctos) {
+			popup.Bien ();
+		}
+		if (opciones == clickeados && opciones != correctos) 
+		{
+			popup.Mal ();
+		}
+		
+	}
+	bool checkeo()
+	{
+		for (int i = 0; i < array.Length; i++) {
+			aux = array [i];
+			if (aux.gano) {
+				return false;
+			}
+
+		}
+
+		return true;
+	}
+
+}
---
+++ matematica_primer_grado/Assets/Scripts/scriptejercicio15.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class scriptejercicio15 : MonoBehaviour {
+	public bool gano = false;
+	private checkewinejercicio15 aux;
+
+	// Use this for initialization
+	void Start () {
+		aux = FindObjectOfType<checkewinejercicio15>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void Desactivar()
+	{
+		this.transform.parent.transform.GetChild (0).GetComponent<Button> ().interactable = false;
+		this.transform.parent.transform.GetChild (1).GetComponent<Button> ().interactable = false;
+		this.transform.parent.transform.GetChild (2).GetComponent<Button> ().interactable = false;
+
+	}
+	public void Tareaclick()
+	{  
+		aux.clickeados++;
+		if (gano == true)
+		{
+			aux.correctos++;
+		}
+		Desactivar ();
+		//transform.parent.GetChild (0).GetComponent<Button> ().interactable = false;
+		//transform.parent.GetChild (1).GetComponent<Button> ().interactable = false;
+		//this.GetComponent<Button>().interactable = false;
+		//aux.valorClickeado = 2;
+
+	}
+}
