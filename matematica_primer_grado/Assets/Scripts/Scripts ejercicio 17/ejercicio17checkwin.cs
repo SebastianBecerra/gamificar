a2bde9ebe0954e4649eaebaeed90bdbca464f364
ClaudioOlivares <claudio_05_028@hotmail.com>
2018-03-31 00:33:19 -0300

    Ejercicio 17
    Se agrego el ejercicio 17

---
+++ matematica_primer_grado/Assets/Scripts/Scripts ejercicio 17/ejercicio17checkwin.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ejercicio17checkwin : MonoBehaviour {
+	public int opciones;
+	public int correctos;
+	private popUpScript popup;
+	// Use this for initialization
+	void Start () {
+		popup = FindObjectOfType<popUpScript> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if (opciones == correctos)
+		{
+			popup.Bien ();
+		}
+
+	}
+}
