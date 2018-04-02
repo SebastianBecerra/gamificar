ec9fbd057014ca0b79642e15386c849dc5955057
Diaz J. Mauricio <ryuma.diaz@gmail.com>
2018-04-02 19:47:00 -0300

    Ejercicio 19 completo
    ejercicio de sumar y restar 1 completo

---
+++ matematica_primer_grado/Assets/Scripts/Scripts ejercicio 19/MatM1eje19.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MatM1eje19 : MonoBehaviour {
+
+    ControllerInputText controller;
+    popUpScript popUp;
+    public int dificultad;
+    public Text numeroPrincipal;
+    int numeroEntero;
+    public Text cuentaParaAnterior;
+    public Text cuentaParaSiguiente;
+    public InputTextScript inputAnterior;
+    public InputTextScript inputSiguiente;
+    public Button botonEstaBien;
+
+    public Image[] imagenesDeFace;
+    public Sprite spriteIncorrecto;
+    public Sprite spriteCorrecto;
+    int face;
+
+	// Use this for initialization
+	void Start(){
+        controller = FindObjectOfType<ControllerInputText>();
+        popUp = FindObjectOfType<popUpScript>();
+
+        face = 0;
+
+        construir();
+    }
+    public void checkeo() {
+        if (controller.EsCorrecto()) {
+            if (face == 10){
+                popUp.Bien();
+            }
+            else {
+                popUp.cartelAcierto();
+                imagenesDeFace[face].sprite = spriteCorrecto;
+                face++;
+            }
+        }
+        else {
+            popUp.cartelError();
+            if (face > 0) {
+                face--;
+                imagenesDeFace[face].sprite = spriteIncorrecto;
+            }
+            else {
+
+            }
+        }
+        construir();
+    }
+
+    void construir() {
+        inputAnterior.gameObject.GetComponent<InputField>().text = "";
+        inputSiguiente.gameObject.GetComponent<InputField>().text = "";
+
+        numeroEntero = Random.Range(2, dificultad);
+        numeroPrincipal.text = numeroEntero.ToString();
+        cuentaParaAnterior.text = numeroEntero + " - 1 = ";
+        cuentaParaSiguiente.text = numeroEntero + " + 1 = ";
+        inputAnterior.valoresCorrectos[0] = (numeroEntero - 1).ToString();
+        inputSiguiente.valoresCorrectos[0] = (numeroEntero + 1).ToString();
+    }
+
+}
