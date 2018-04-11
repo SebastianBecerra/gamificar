3127f8763c025ab2da24c686d9412060d5d2145e
Diaz J. Mauricio <ryuma.diaz@gmail.com>
2018-04-11 20:51:54 -0300

    Ejercicio 4 / fondos
    Ejercicio 4 retocado
    - Solucionado DaD
    - Retoque visual
    - Solucionada funcionalidad
    Agregado nuevos fondos

--- matematica_primer_grado/Assets/Scripts/Scripts ejercicio 4/ScriptEjercicio4matmod1.cs
+++ matematica_primer_grado/Assets/Scripts/Scripts ejercicio 4/ScriptEjercicio4matmod1.cs
@@ -5,35 +5,29 @@ using UnityEngine;
 public class ScriptEjercicio4matmod1 : MonoBehaviour {
 	// Script para confirmar si estaw correcto el ejercicio
 	//Contiene la funcion que va a usar el boton de checkeo
-	public DropZone[] array;
-	public bool gano;
+	DropZone[] dropZones;
 	private popUpScript popup;
 	public int cantidad;
 	// Use this for initialization
 	void Start () {
 		popup = FindObjectOfType<popUpScript> ();
-		array = FindObjectsOfType<DropZone> ();
+		dropZones = FindObjectsOfType<DropZone> ();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
 	public void checkearRepartir() // funcion del bonton
 	{
-		foreach (DropZone a in array) {  
-			if (a.name == "DropZone") {
-
-				if (a.transform.childCount == cantidad) {
-					gano = true;
-					popup.Bien ();
-
-				} else {
-					gano = false;
-					popup.Mal ();
-				}
-			}
+        bool estado = true;
+		foreach (DropZone dz in dropZones) {
+            if (!dz.ignorarCheckeo) {
+                estado = estado && (dz.transform.childCount == cantidad);
+            }
 		}
+        if (estado){
+            popup.Bien();
+        }
+        else {
+            popup.Mal();
+        }
 	}
 
 }
