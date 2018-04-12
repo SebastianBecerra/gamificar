6d2c6379c92bb953c69fcc96d3bf24be66745b8e
ClaudioOlivares <claudio_05_028@hotmail.com>
2018-04-12 12:08:54 -0300

    Ejercicio 32
    Se agrego Ejercicio32

---
+++ matematica_primer_grado_2do_modulo/matematica_primer_grado_2/Assets/Scenes/Ejercicio32/Actividad32Checkwinscript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Actividad32Checkwinscript : MonoBehaviour {
+	public string [] arrayCorrecto;
+	public GameObject dropzone;
+	private popUpScript popup;
+	// Use this for initialization
+	void Start () {
+		popup = FindObjectOfType<popUpScript> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	public void checkeo()
+	{
+		if (dropzone.transform.childCount == 8) {
+			for (int i = 0; i < arrayCorrecto.Length; i++) {
+
+				if (arrayCorrecto [i] != dropzone.transform.GetChild (i).name) {
+					popup.Mal ();
+
+				}
+
+			}
+
+		} else
+		{
+			popup.Mal ();
+		}
+		popup.Bien();
+	}
+	public void escorrecto()
+	{
+       
+	}
+}
