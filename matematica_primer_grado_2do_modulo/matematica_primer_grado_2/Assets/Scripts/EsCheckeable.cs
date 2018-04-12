78e0cabfa35222aea2d30b941487cef948f71827
ClaudioOlivares <claudio_05_028@hotmail.com>
2018-04-12 08:47:59 -0300

    Ejercicios incompletos

---
+++ matematica_primer_grado_2do_modulo/matematica_primer_grado_2/Assets/Scripts/EsCheckeable.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public  abstract class EsCheckeable : MonoBehaviour {
+
+    public abstract bool EsCorrecto();
+}
---
+++ matematica_primer_grado_2do_modulo/matematica_primer_grado_2/Assets/Scripts/GameController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+    [HideInInspector] public List< EsCheckeable> chequear = new List<EsCheckeable>();
+    [HideInInspector] public popUpScript popUp;
+
+	// Use this for initialization
+	void Start () {
+        popUp = GameObject.FindObjectOfType<popUpScript>();
+	}
+    public void checkearEstado() {
+        if (chequear.Count != 0) {
+            bool estado = true;
+            for (int i = 0; i < chequear.Count; i++) {
+                estado = estado && chequear[i].EsCorrecto();
+            }
+            if (estado) {
+                invocarPopUp();
+            }
+        }
+        else {
+            Debug.LogError("Es imposible chequear el estado del juego porque no se encontro ningun [EsChequeable] en la lista chequear");
+        }
+    }
+    public void invocarPopUp() {
+        Debug.Log("win");
+        if (popUp != null) {
+            popUp.Bien();
+        }
+    }
+}
