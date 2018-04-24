62ea160e32ae99e702ddcf967c59103e6c647ded
Sebastian Becerra <SebastianBecerra@users.noreply.github.com>
2018-04-24 00:12:37 -0300

    Ejercicio 31
    Funcionalidad completa ejercicio 31, faltan retoques visuales.

---
+++ matematica_primer_grado_2do_modulo/matematica_primer_grado_2/Assets/Scenes/Ejercicio31/script/ej31mod2_2.cs
@@ -0,0 +1,317 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ej31mod2_2 : MonoBehaviour, IDropHandler {
+
+    public GameObject lugarDrop, huella1, huella2, huella3, cubo, cilindro, piramide;
+    private int id, randomSelect, estado;
+    popUpScript popUp;
+    private bool ultimoObjeto;
+
+	// Use this for initialization
+	void Start () {
+        popUp = FindObjectOfType<popUpScript>();
+        huella1.SetActive(false);
+        huella2.SetActive(false);
+        huella3.SetActive(false);
+        estado = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        Invoke("getChild", 0.1f);
+        Invoke("huella", 0.1f);
+    }
+
+    void getChild()
+    {
+        id = lugarDrop.transform.GetChild(0).GetComponent<Dragable>().idDropZone1;
+        switch (id)
+        {
+            case 1:
+                Debug.Log("cilindro");
+                break;
+            case 2:
+                Debug.Log("cubo");
+                break;
+            case 3:
+                Debug.Log("piramide");
+                break;
+        }
+    }
+
+    void huella()
+    {
+        if (id == 1)
+        {
+            randomSelect = Random.Range(1, 3);
+            switch (randomSelect)
+            {
+                case 1:
+                    huella1.SetActive(true);
+                    huella2.SetActive(false);
+                    huella3.SetActive(false);
+                    break;
+                case 2:
+                    huella1.SetActive(false);
+                    huella2.SetActive(true);
+                    huella3.SetActive(false);
+                    break;
+            }
+        }
+        if (id == 2)
+        {
+            randomSelect = Random.Range(1, 3);
+            switch (randomSelect)
+            {
+                case 1:
+                    huella1.SetActive(false);
+                    huella2.SetActive(false);
+                    huella3.SetActive(true);
+                    break;
+                case 2:
+                    huella1.SetActive(false);
+                    huella2.SetActive(true);
+                    huella3.SetActive(false);
+                    break;
+            }
+        }
+        if (id == 3)
+        {
+            randomSelect = Random.Range(1, 3);
+            switch (randomSelect)
+            {
+                case 1:
+                    huella1.SetActive(false);
+                    huella2.SetActive(false);
+                    huella3.SetActive(true);
+                    break;
+                case 2:
+                    huella1.SetActive(true);
+                    huella2.SetActive(false);
+                    huella3.SetActive(false);
+                    break;
+            }
+        }
+    }
+
+    public void checkHuellaSi()
+    {
+        switch (id)
+        {
+            case 1:
+                if (!ultimoObjeto)
+                {
+                    if (huella1.activeInHierarchy)
+                    {
+                        popUp.cartelAcierto();
+                        estado++;
+                        huella1.SetActive(false);
+                        Destroy(cilindro);
+                        if (estado == 2)
+                        {
+                            Invoke("ultimo", 0.2f);
+                        }
+                    }
+                    else
+                    {
+                        popUp.Mal();
+                    }
+                }
+                else
+                {
+                    if (huella1.activeInHierarchy)
+                    {
+                        popUp.Bien();
+                    }
+                    else
+                    {
+                        popUp.Mal();
+                    }
+                }
+                break;
+            case 2:
+                if (!ultimoObjeto)
+                {
+                    if (huella2.activeInHierarchy)
+                    {
+                        popUp.cartelAcierto();
+                        estado++;
+                        huella2.SetActive(false);
+                        Destroy(cubo);
+                        if (estado == 2)
+                        {
+                            Invoke("ultimo", 0.2f);
+                        }
+                    }
+                    else
+                    {
+                        popUp.Mal();
+                    }
+                }
+                else
+                {
+                    if (huella2.activeInHierarchy)
+                    {
+                        popUp.Bien();
+                    }
+                    else
+                    {
+                        popUp.Mal();
+                    }
+                }
+                break;
+            case 3:
+                if (!ultimoObjeto)
+                {
+                    if (huella3.activeInHierarchy)
+                    {
+                        popUp.cartelAcierto();
+                        estado++;
+                        huella3.SetActive(false);
+                        Destroy(piramide);
+                        if (estado == 2)
+                        {
+                            Invoke("ultimo", 0.2f);
+                        }
+                    }
+                    else
+                    {
+                        popUp.Mal();
+                    }
+                }
+                else
+                {
+
+
+                    if (huella3.activeInHierarchy)
+                    {
+                        popUp.Bien();
+                    }
+                    else
+                    {
+                        popUp.Mal();
+                    }
+                }
+                break;
+        }
+    }
+
+
+
+    public void checkHuellaNo()
+    {
+        switch (id)
+        {
+            case 1:
+                if (!ultimoObjeto)
+                {
+                    if (!huella1.activeInHierarchy)
+                    {
+                        popUp.cartelAcierto();
+                        estado++;
+                        huella2.SetActive(false);
+                        huella3.SetActive(false);
+                        Destroy(cilindro);
+                        if (estado == 2)
+                        {
+                            Invoke("ultimo", 0.2f);
+                        }
+                    }
+                    else
+                    {
+                        popUp.Mal();
+                    }
+                }
+                else
+                {
+                    if (!huella1.activeInHierarchy)
+                    {
+                        popUp.Bien();
+                    }
+                    else
+                    {
+                        popUp.Mal();
+                    }
+                }
+                break;
+            case 2:
+                if (!ultimoObjeto)
+                {
+                    if (!huella2.activeInHierarchy)
+                    {
+                        popUp.cartelAcierto();
+                        estado++;
+                        huella1.SetActive(false);
+                        huella3.SetActive(false);
+                        Destroy(cubo);
+                        if (estado == 2)
+                        {
+                            Invoke("ultimo", 0.2f);
+                        }
+                    }
+                    else
+                    {
+                        popUp.Mal();
+                    }
+                }
+                else
+                {
+                    if (!huella2.activeInHierarchy)
+                    {
+                        popUp.Bien();
+                    }
+                    else
+                    {
+                        popUp.Mal();
+                    }
+                }
+                break;
+            case 3:
+                if (!ultimoObjeto)
+                {
+                    if (!huella3.activeInHierarchy)
+                    {
+                        popUp.cartelAcierto();
+                        estado++;
+                        huella1.SetActive(false);
+                        huella2.SetActive(false);
+                        Destroy(piramide);
+                        if (estado == 2)
+                        {
+                            Invoke("ultimo", 0.2f);
+                        }
+                    }
+                    else
+                    {
+                        popUp.Mal();
+                    }
+                }
+                else
+                {
+                    if (!huella3.activeInHierarchy)
+                    {
+                        popUp.Bien();
+                    }
+                    else
+                    {
+                        popUp.Mal();
+                    }
+                }
+                break;
+        }
+    }
+
+
+    void ultimo()
+    {
+        ultimoObjeto = true;
+    }
+}
---
+++ matematica_primer_grado_2do_modulo/matematica_primer_grado_2/Assets/Scenes/Ejercicio31/script/ej31mod2controller.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ej31mod2controller : MonoBehaviour {
+
+    public GameObject pos1, pos2, pos3, fig1, fig2, fig3, drop1, drop2, drop3;
+    private int randomSelect;
+    popUpScript popUp;
+    public string escenaSiguiente;
+
+
+	// Use this for initialization
+	void Start () {
+        popUp = FindObjectOfType<popUpScript>();
+        randomSelect = Random.Range(1, 4);
+        switch (randomSelect)
+        {
+            case 1:
+                fig1.transform.position = pos1.transform.position;
+                fig2.transform.position = pos2.transform.position;
+                fig3.transform.position = pos3.transform.position;
+                drop1.GetComponent<DropZone>().idDropZone = 1;
+                drop2.GetComponent<DropZone>().idDropZone = 2;
+                drop3.GetComponent<DropZone>().idDropZone = 3;
+                break;
+            case 2:
+                fig1.transform.position = pos3.transform.position;
+                fig2.transform.position = pos2.transform.position;
+                fig3.transform.position = pos1.transform.position;
+                drop1.GetComponent<DropZone>().idDropZone = 3;
+                drop2.GetComponent<DropZone>().idDropZone = 2;
+                drop3.GetComponent<DropZone>().idDropZone = 1;
+                break;
+            case 3:
+                fig1.transform.position = pos2.transform.position;
+                fig2.transform.position = pos3.transform.position;
+                fig3.transform.position = pos1.transform.position;
+                drop1.GetComponent<DropZone>().idDropZone = 3;
+                drop2.GetComponent<DropZone>().idDropZone = 1;
+                drop3.GetComponent<DropZone>().idDropZone = 2;
+                break;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void checkDrops()
+    {
+        if (drop1.GetComponent<DropZone>().idDropZone == drop1.GetComponentInChildren<Dragable>().idDropZone1)
+        {
+            if (drop2.GetComponent<DropZone>().idDropZone == drop2.GetComponentInChildren<Dragable>().idDropZone1)
+            {
+                if (drop3.GetComponent<DropZone>().idDropZone == drop3.GetComponentInChildren<Dragable>().idDropZone1)
+                {
+                    popUp.cartelAcierto();//muestra el cartel de acierto
+                    Invoke("siguienteEscena", 0.5f);//y carga la siguiente escena
+                }
+                else
+                {
+                    popUp.Mal();
+                }
+            }
+            else
+            {
+                popUp.Mal();
+            }
+        }
+        else
+        {
+            popUp.Mal();
+        }
+    }
+
+    void siguienteEscena()
+    {
+        SceneManager.LoadScene(escenaSiguiente);
+    }
+}
