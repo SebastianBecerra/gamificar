f1039b482fa7645b8b8146384cf4ae9ef7f08b8d
Diaz J. Mauricio <ryuma.diaz@gmail.com>
2018-04-24 12:13:37 -0300

    fondos

---
+++ matematica_primer_grado_2do_modulo/matematica_primer_grado_2/Assets/Scenes/Ejercicio35/cartaEjer35.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class cartaEjer35 : MonoBehaviour {
+
+    public Image dorso;
+    public int id;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
