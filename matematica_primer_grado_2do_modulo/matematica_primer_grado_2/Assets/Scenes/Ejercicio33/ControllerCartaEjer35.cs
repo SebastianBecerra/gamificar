37a95c588b386aa163df6b2aeb94642186ac7ef5
Diaz J. Mauricio <ryuma.diaz@gmail.com>
2018-04-25 11:54:13 -0300

    ejer 33-4
    a duras penas

---
+++ matematica_primer_grado_2do_modulo/matematica_primer_grado_2/Assets/Scenes/Ejercicio33/ControllerCartaEjer35.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerCartaEjer35 : MonoBehaviour {
+
+    cartaEjer35[] cartas;
+    bool esperandoSegunda;
+    cartaEjer35 primeraCarta;
+    cartaEjer35 segundaCarta;
+    popUpScript popUp;
+    int count;
+
+    // Use this for initialization
+    void Start () {
+        count = 0;
+        buscarCartas();
+        popUp = FindObjectOfType<popUpScript>();
+	}
+    void buscarCartas() {
+        cartas = FindObjectsOfType<cartaEjer35>();
+    }
+
+    public void habilitarBanderas() {
+        foreach (cartaEjer35 c in cartas){
+            if (c.bocaArriba == false)
+            {
+                c.bandera = true;
+            }
+            
+        }
+    }
+    public void desabilitarBanderas()
+    {
+        foreach (cartaEjer35 c in cartas)
+        {
+            c.bandera = false;
+        }
+    }
+
+    public void Contralador(cartaEjer35 carta) {
+        
+
+        if (esperandoSegunda)
+        {
+            segundaCarta = carta;
+            //desabilitarBanderas();
+            if (primeraCarta.id == carta.id)
+            {
+                buscarCartas();
+                if (count == 4)
+                {
+                    popUp.Bien();
+                }
+                else
+                {
+                    popUp.cartelAcierto();
+                    count++;
+                }
+            }
+            else {
+                Invoke("darVueltas", 1.0f);
+                popUp.cartelError();
+
+            }
+            //habilitarBanderas();
+            esperandoSegunda = false;
+        }
+        else {
+            primeraCarta = carta;
+            //primeraCarta.bandera = false;
+            esperandoSegunda = true;
+        }
+    }
+    void darVueltas() {
+        primeraCarta.darVuelta();
+        primeraCarta.bandera = true;
+        segundaCarta.darVuelta();
+        segundaCarta.bandera = true;
+    }
+}
---
+++ matematica_primer_grado_2do_modulo/matematica_primer_grado_2/Assets/Scenes/Ejercicio33/cartaEjer35.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class cartaEjer35 : MonoBehaviour {
+
+    public Image dorso;
+    public int id;
+    public bool bocaArriba;
+    [HideInInspector] public bool bandera;
+    [HideInInspector] public bool volteable;
+    Transform miTransform;
+    ControllerCartaEjer35 controller;
+	// Use this for initialization
+	void Start () {
+        bandera = true;
+        miTransform = gameObject.GetComponent<Transform>();
+        controller = FindObjectOfType<ControllerCartaEjer35>();
+        
+    }
+    public void darVuelta() {
+        StartCoroutine("Voltear");
+    }
+
+    void OnMouseDown()
+    {
+        if (bandera)
+        {
+            controller.Contralador(this);
+            darVuelta();
+        }
+    }
+    // Update is called once per frame
+    void Update () {
+        bocaArriba = !dorso.enabled;
+    }
+    IEnumerator Voltear()
+    {
+        controller.desabilitarBanderas();
+        
+        while(miTransform.localScale.x > 0)
+        {
+            miTransform.localScale -= new Vector3(0.1f, 0, 0);
+            yield return new WaitForSeconds(.01f);
+        }
+        dorso.enabled = !dorso.enabled;
+       // bocaArriba = !dorso.enabled;
+        while (miTransform.localScale.x < 1)
+        {
+            miTransform.localScale += new Vector3(0.1f, 0, 0);
+            yield return new WaitForSeconds(.01f);
+        }
+        controller.habilitarBanderas();
+
+    }
+}
