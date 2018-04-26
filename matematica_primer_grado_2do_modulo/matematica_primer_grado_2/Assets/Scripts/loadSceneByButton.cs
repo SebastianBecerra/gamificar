f18e39f04cb2dde3fe5d263411ce156c812f3bef
Sebastian Becerra <SebastianBecerra@users.noreply.github.com>
2018-04-26 00:47:22 -0300

    Menu
    Agregado escena menu

---
+++ matematica_primer_grado_2do_modulo/matematica_primer_grado_2/Assets/Scripts/loadSceneByButton.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class loadSceneByButton : MonoBehaviour {
+
+    //script con la funcionalidad de abrir la escene que corresponda en el menu
+    public string escena;
+
+    private Button btn;//referencia al boton 
+
+	// Use this for initialization
+	void Start () {
+        btn = gameObject.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+	}
+	
+    //abre escena con el nombre del boton
+    public void abrirEscena()
+    {
+        SceneManager.LoadScene(escena);
+    }
+
+
+	void TaskOnClick()
+    {
+        abrirEscena();
+    }
+}
